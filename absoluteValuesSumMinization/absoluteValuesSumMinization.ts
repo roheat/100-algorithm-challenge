0cc8af17be3b5314d23792b45f98fa555a218315
Rohith Srivathsav <rohithsrivathsav@gmail.com>
2020-04-06 00:43:29 +0530

    solved problem 1

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,12 @@
 function absoluteValuesSumMinimization(a: number[]): number {
-
+  const isEven = a.length % 2 === 0;
+  if (isEven) {
+    return a[a.length / 2 - 1];
+  }
+  return a[(a.length - 1) / 2];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
